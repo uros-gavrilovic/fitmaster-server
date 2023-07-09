5448209b4c80987e804c7e31b806d7c613cb6886
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-07-09 13:04:47 +0200

    init

---
+++ src/main/java/com/mastercode/fitmaster/FitMaster.java
@@ -0,0 +1,10 @@
+package com.mastercode.fitmaster;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMaster {
+    public static void main(String[] args) {
+        SpringApplication.run(FitMaster.class, args);
+    }
+}
---
+++ src/test/java/com/mastercode/fitmaster/FitMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercode.fitmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
