96cd4c3c79a6d48050092a599d70b46ade334a2d
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-10-17 11:27:22 +0200

    Enabled CORS.

--- src/main/java/com/mastercode/fitmaster/config/CorsConfig.java
+++ src/main/java/com/mastercode/fitmaster/config/CorsConfig.java
@@ -4,20 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
-    public UrlBasedCorsConfigurationSource configureCORS() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://delightful-stone-0033d2303.3.azurestaticapps.net"); // Front-End URL
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
