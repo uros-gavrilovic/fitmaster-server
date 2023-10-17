a8d224c95cb1125c7133f784726ee168a0072204
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-10-18 00:20:46 +0200

    Minor tweaks to CORS.

--- src/main/java/com/mastercode/fitmaster/config/WebConfig.java
+++ src/main/java/com/mastercode/fitmaster/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.mastercode.fitmaster.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -8,33 +7,32 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig {
 
     @Bean
-    public FilterRegistrationBean corsFilter() {
-        //specify all what is coming from frontend
+    public CorsFilter corsFilter() {
+        // Specify all what is coming from the frontend
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
+
         configuration.setAllowCredentials(true);
         configuration.addAllowedOrigin("http://localhost:3000"); // React frontend
         configuration.addAllowedOrigin("http://localhost:19006"); // Mobile application
-        configuration.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
-        configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name(), HttpMethod.PUT.name()));
-        //Time the Options request is accepted
-        configuration.setMaxAge(3600L); //30 min
+        configuration.addAllowedOrigin(
+                "https://delightful-stone-0033d2303.3.azurestaticapps.net"); // Front-End URL (Azure)
+        configuration.setAllowedHeaders(
+                Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
+        configuration.setAllowedMethods(
+                Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name(),
+                        HttpMethod.PUT.name()));
+        // Time the Options request is accepted
+        configuration.setMaxAge(3600L); // 30 min
+
         source.registerCorsConfiguration("/**", configuration);
-        //applied for all the requests
-
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-
-        bean.setOrder(-102); //executes before any Bean
-        return bean;
+        return new CorsFilter(source);
     }
-
 }
