837e0935630cb26c34e304f19b30e2d4d496d0bc
Uros Gavrilovic <urosgavrilovic77@gmail.com>
2024-07-13 12:17:21 +0200

    Code format

--- src/main/resources/database/migrations/V09__insert_admin.sql
+++ src/main/resources/database/migrations/V09__insert_admin.sql
@@ -1,3 +1,5 @@
+DELETE FROM trainer WHERE id = 0;
+
 INSERT INTO trainer(id, first_name, last_name, username, email, email_verified, password)
 VALUES (0, 'Admin', 'Admin', 'admin', 'admin@fitmaster.com', true,
         '$2a$10$DckRmrkx7D6nwVpB6juCB..ekyqwfqRMHdN0Kx.11UWbGndMacrUa');
